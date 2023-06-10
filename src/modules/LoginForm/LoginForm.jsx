3481ccf79a19770cea0581670c3d02658d1fc1ad
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-06-10 13:15:55 +0300

    adds LoginForm validation

--- src/modules/LoginForm/LoginForm.jsx
+++ src/modules/LoginForm/LoginForm.jsx
@@ -7,17 +7,29 @@ import useForm from '../../hooks/useForm';
 import initialState from './initialState';
 import { FormWrapper } from './LoginForm.styled';
 import { selectIsLoading } from '../../redux/auth/selectors';
+import { useEffect, useState } from 'react';
 
 const LoginForm = ({ onSubmit, displaySpinner = false }) => {
   const isLoading = useSelector(selectIsLoading);
   const { state, handleChange, handleSubmit } = useForm({ initialState, onSubmit });
   const { email, password } = state;
 
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+  useEffect(() => {
+    const emailRegexp =
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+    setIsEmailValid(emailRegexp.test(email));
+    setIsPasswordValid(password.length > 5);
+  }, [email, password]);
+
   return (
     <FormWrapper onSubmit={handleSubmit}>
       <TextField value={email} onChange={handleChange} {...fields.email} />
       <TextField value={password} onChange={handleChange} {...fields.password} />
-      <Button disabled={isLoading}>
+      <Button disabled={isLoading || !isEmailValid || !isPasswordValid}>
         {displaySpinner && (
           <Loader size={22} position="absolute" top="15px" left="96px" />
         )}
